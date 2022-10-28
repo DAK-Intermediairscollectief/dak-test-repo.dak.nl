4e31bf69f48c7dd3bb597f7f595d2fe4de50524f
BokB <BokB@ASK040726.mpnet.missingpiece.nl>
2022-10-28 16:01:12 +0200

    2 regels toegevoegd

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "test repo github";
-        ?>
-    </body>
+   <head>
+      <meta charset="UTF-8">
+      <title></title>
+   </head>
+   <body>
+      <?php
+         echo "test repo github";
+         echo "br /<><br />";
+         echo "aanpassingen toegevoegd";
+       ?>
+   </body>
 </html>
